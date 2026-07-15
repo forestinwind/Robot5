34fa0c8c1b6da3e03eae6b708ddb450c177c0c56
surface <surface@DESKTOP-SPN7JON>
2026-07-15 15:21:13 +0800

    添加项目文件。

---
+++ Robot5/Robot.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#include"MCSL_Fun.h"
+#include"MCSL.h"
+#include"kinematics.h"
+#include"EcmDriver.h"
---
+++ Robot5/main.cpp
@@ -0,0 +1,6 @@
+﻿#include<iostream>
+#include"Robot.h"
+int main()
+{
+	return 0;
+}
