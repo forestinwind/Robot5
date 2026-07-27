905e2710537a6cfe0fdb8b8ceebe4942f4536c02
huurinn <1918603642@qq.com>
2026-07-27 14:37:12 +0800

    [add]

--- src/MoveCommander.h
+++ src/MoveCommander.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include"Robot.h"
-
+#include<Eigen/Dense>
 #include<iostream>
 namespace RinnRobotCommander{
     class MoveCommander{
     private:
+        static Eigen::Matrix3d eulerZYXToRotation(double rxDeg, double ryDeg, double rzDeg) {
+            const double deg2rad = std::acos(-1.0) / 180.0;
+            const double rx = rxDeg * deg2rad;
+            const double ry = ryDeg * deg2rad;
+            const double rz = rzDeg * deg2rad;
+
+            auto Rz = [](double a) { return Eigen::AngleAxisd(a, Eigen::Vector3d::UnitZ()); };
+            auto Ry = [](double a) { return Eigen::AngleAxisd(a, Eigen::Vector3d::UnitY()); };
+
+            return (Rz(rz) * Ry(ry)).toRotationMatrix();
+        }
+        static const Eigen::Vector3d moveDir;
+        static Eigen::Vector3d getFlangeLoc(Eigen::Vector3d dir, Eigen::Vector3d p,Eigen::Vector3d seenDir = moveDir){
+            Eigen::Matrix3d mat = eulerZYXToRotation(dir.x(), dir.y(), dir.z());
+            return p + mat * seenDir;
+        }
+        static Eigen::Vector3d deFlangeLoc(Eigen::Vector3d dir, Eigen::Vector3d p,Eigen::Vector3d seenDir = moveDir){
+            Eigen::Matrix3d mat = eulerZYXToRotation(dir.x(), dir.y(), dir.z());
+            return p + mat * -seenDir;
+        }
         double tool_Pos[3];
         double tool_Speed[6];
         double wRPM;
@@ -35,6 +55,10 @@ namespace RinnRobotCommander{
         void MoveIntime(double dx,double dy,double dz,double drx,double dry,double drz,double sec){
             JointPositions *pos = new JointPositions();
             controlSystem::GetCurJPos(pos);
+            Eigen::Vector3d tipPos = getFlangeLoc(Eigen::Vector3d(rx,ry,rz),Eigen::Vector3d(x,y,z));
+            Eigen::Vector3d targetPos = tipPos + Eigen::Vector3d(dx,dy,dz);
+            Eigen::Vector3d targetDir = Eigen::Vector3d(rx + drx, ry + dry, rz + drz);
+            Eigen::Vector3d targetFlangePos = deFlangeLoc(targetDir,targetPos);
             SetSpeed(dx/sec,dy/sec,dz/sec,drx/sec,dry/sec,drz/sec);
             std::cout<<"rated: " << dx / sec << ", " << dy / sec << ", " << dz / sec << ", " << drx / sec << ", " << dry / sec << ", " << drz / sec << std::endl;
             pos->j1 =dx, pos->j2 = dy, pos->j3 = dz, pos->j4 = drx, pos->j5 = dry, pos->j6 = drz;
@@ -44,5 +68,6 @@ namespace RinnRobotCommander{
         ~MoveCommander(){
             controlSystem::CloseSystem();
         }
-    }; 
+    };
+    const Eigen::Vector3d MoveCommander::moveDir(1.05,0.69,-504.41);
 }
